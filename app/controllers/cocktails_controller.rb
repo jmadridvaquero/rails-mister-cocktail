955746949cbc55f96296dbdbc2831f3eea578ce9
Javier Madrid <jmadridvaquero@gmail.com>
2019-05-17 14:01:07 -0400

    functioning app. Still some css components to work on before deployment into production

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,11 +1,11 @@
 class CocktailsController < ApplicationController
 
 	def index
-		@cocktail = Cocktail.all
+		@cocktails = Cocktail.all
 	end
-	def new
-		@new_cocktail = Cocktail.new
 
+	def new
+		@cocktail = Cocktail.new
 	end
 
 	def show
@@ -14,16 +14,39 @@ class CocktailsController < ApplicationController
 
 
 	def create
-		@new_cocktail = Cocktail.new(cocktail_params)
-    if @new_cocktail.save
-      redirect_to cocktail_path(@new_cocktail)
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
     end
 	end
+
+	def edit
+    @cocktail = Cocktail.find(cocktail_params)
+  end
+
+  def update
+    if @cocktail.update(cocktail_params)
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    if @cocktail.destroy
+      redirect_to cocktails_path
+    else
+      render :index
+    end
+  end
 
 	private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :id)
+    params.require(:cocktail).permit(:name, :id )
   end
 
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,18 +1,37 @@
 class DosesController < ApplicationController
+  before_action :find_it, only: [:new, :create]
 
-	def index
-		@dose = Dose.all
-	end
+  def new
+    find_it
+    @dose = Dose.new
+  end
 
-	def new
-		@new_dose = Dose.new
-	end
+  def create
+    find_it
+    @dose = Dose.new(params_doses)
+    @dose.cocktail = @cocktail #this is the extra line you need to associate when N-N
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
 
-	def destroy
-		@dose = Dose.find(params[:id])
-  	@dose.destroy
-  	redirect_to doses_path
-	end
+  def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail.id
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
 
 
+private
+
+  def find_it
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def params_doses
+    params.require(:dose).permit(:ingredient_id, :description)
+  end
 end
